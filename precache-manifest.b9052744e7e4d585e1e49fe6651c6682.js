8cffee97a3fc9551e1c1260c8e9e63b0f53e719e
bickkysahani <bickkysahani@gmail.com>
2022-01-15 17:29:43 +0545

    Updates

---
+++ precache-manifest.b9052744e7e4d585e1e49fe6651c6682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155514288fe32313ba0380bf13a2936a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "185d53eed09bf2fb3a38",
+    "url": "/portfolio/static/css/main.213e782c.chunk.css"
+  },
+  {
+    "revision": "4f5a83dfa138934948d4",
+    "url": "/portfolio/static/js/2.b284396f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/portfolio/static/js/2.b284396f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185d53eed09bf2fb3a38",
+    "url": "/portfolio/static/js/main.c8cfafc3.chunk.js"
+  },
+  {
+    "revision": "fbc205e655803b991987",
+    "url": "/portfolio/static/js/runtime-main.142586e4.js"
+  }
+]);
